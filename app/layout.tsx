fc5281e92d1f26042ddde6885191e9fd2470a8ea
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2025-07-13 16:11:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         <main className="min-h-screen">
           {children}
